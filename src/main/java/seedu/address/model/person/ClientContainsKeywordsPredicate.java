815ecca1b0b825f8c2d9b794d459a3a8c99ecd21
Abhijay <abhijay3852@gmail.com>
2025-10-09 21:37:39 +0800

    Update Find command to search for tags

---
+++ src/main/java/seedu/address/model/person/ClientContainsKeywordsPredicate.java
@@ -0,0 +1,62 @@
+package seedu.address.model.person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+import seedu.address.commons.util.ToStringBuilder;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.  
+ */public class ClientContainsKeywordsPredicate implements Predicate<Person> {
+    public enum SearchType { NAME, TAG, DATE}
+
+    private final List<String> keywords;
+    private final SearchType type;
+
+    public ClientContainsKeywordsPredicate(SearchType type, List<String> keywords) {
+        this.keywords = keywords;
+        this.type = type;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        switch (type) {
+
+            case NAME:
+                return keywords.stream()
+                        .anyMatch(keyword ->
+                                person.getName().fullName.toLowerCase().contains(keyword.toLowerCase()));
+
+            case TAG:
+                return person.getTags().stream()
+                        .map(tag -> tag.tagName.toLowerCase())
+                        .anyMatch(tagName ->
+                                keywords.stream()
+                                        .anyMatch(keyword -> tagName.equals(keyword.toLowerCase())));
+
+        }
+        return false;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls  
+        if (!(other instanceof NameContainsKeywordsPredicate)) {
+            return false;
+        }
+
+        ClientContainsKeywordsPredicate otherNameContainsKeywordsPredicate = (ClientContainsKeywordsPredicate) other;
+        return keywords.equals(otherNameContainsKeywordsPredicate.keywords);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).add("keywords", keywords).toString();
+    }
+}
